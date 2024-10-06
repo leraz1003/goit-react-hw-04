8b9c3a95a99df5635870ea5f2cb1c96b00339b91
Valeriia Zubrytska <valeriia.zubrytska@gmail.com>
2024-10-06 21:02:18 +0300

    add searchBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import ErrorMessage from "./components/ErrorMessage/ErrorMessage";
+import ImageGallery from "./components/ImageGallery/ImageGallery";
+import ImageModal from "./components/ImageModal/ImageModal";
+import Loader from "./components/Loader/Loader";
+import LoadMoreBtn from "./components/LoadMoreBtn/LoadMoreBtn";
+import SearchBar from "./components/SearchBar/SearchBar";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    console.log("submit");
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <LoadMoreBtn />
+      <SearchBar onSubmit={handleSubmit} />
+      <ImageGallery />
+      <Loader />
+      <ErrorMessage />
+      <ImageModal />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,5 +1,19 @@
-const SearchBar = () => {
-  return <div>SearchBar</div>;
+const SearchBar = ({ onSubmit }) => {
+  return (
+    <header>
+      <form>
+        <input
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+        />
+        <button type="submit" onClick={onSubmit}>
+          Search
+        </button>
+      </form>
+    </header>
+  );
 };
 
 export default SearchBar;
