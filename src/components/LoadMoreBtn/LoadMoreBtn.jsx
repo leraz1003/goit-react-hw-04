69a6a579451adc4ea0c807889b2c4cf061630cdc
Valeriia Zubrytska <valeriia.zubrytska@gmail.com>
2024-10-07 21:42:20 +0300

    add-class btn

--- src/components/LoadMoreBtn/LoadMoreBtn.jsx
+++ src/components/LoadMoreBtn/LoadMoreBtn.jsx
@@ -1,7 +1,7 @@
 import s from "./LoadMoreBtn.module.css";
 const LoadMoreBtn = ({ onClick }) => {
   return (
-    <button type="button" onClick={onClick}>
+    <button type="button" onClick={onClick} className={s.button}>
       Load More
     </button>
   );
